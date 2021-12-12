16486e8a7fff5498b61239442f31a41a870b1cff
Brandon Lin <brandonlin9716@gmail.com>
2021-12-11 20:56:08 -0800

    Implement Selenium test class with test case.

--- src/test/java/com/company/DemoTest.java
+++ src/test/java/com/company/DemoTest.java
@@ -1,13 +1,33 @@
 package com.company;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class DemoTest {
+public class DemoTest {
+
+    static WebDriver driver;
 
     @BeforeClass
-    public void setupDrivers(){}
+    public static void browserOpen() {
+        String chromeDriverLoc = "/Users/brandonlin/Documents/Projects/ExternalLibraries/SeleniumDrivers/chromedriver";
+        System.setProperty("webdriver.chrome.driver", chromeDriverLoc);
+        driver = new ChromeDriver();
+    }
 
+    @Test
+    public void getGoogle() throws Exception{
+        driver.get("https://www.google.ca");
+        String title = driver.getTitle();
+
+        String expectedTitle = "Google";
+        assertEquals(expectedTitle, title);
+    }
+
+    @AfterClass
+    public static void browserClose() {
+        driver.quit();
+    }
 }
