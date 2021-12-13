4004996c4c4968df35034b4a5927642b1fc63e71
Brandon Lin <brandonlin9716@gmail.com>
2021-12-12 17:44:30 -0800

    Implement test getYohji() with Action and Actions class.

--- src/test/java/com/company/DemoTest.java
+++ src/test/java/com/company/DemoTest.java
@@ -1,9 +1,11 @@
 package com.company;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.*;
 import org.junit.*;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -41,11 +43,47 @@ public class DemoTest {
     public void getGoogle() throws InterruptedException {
         driver.get("https://www.google.ca");
         String title = driver.getTitle();
-
         Thread.sleep(2000);
 
         String expectedTitle = "Google";
         assertEquals(expectedTitle, title);
+    }
+
+    @Test
+    public void getYohji() throws InterruptedException {
+        driver.get("https://www.google.ca");
+        Thread.sleep(1000);
+
+        WebElement searchBar = driver.findElement(By.xpath("//body/div[1]/div[3]/form[1]/div[1]/div[1]/div[1]/div[2]/div[2]/input[1]"));
+        WebElement googleSearch = driver.findElement(By.cssSelector("input.gNO89b"));
+        Actions builder = new Actions(driver);
+        Action searchYohji = builder
+                .sendKeys(searchBar, "yohji")
+                .keyDown(googleSearch, Keys.SHIFT)
+                .keyUp(googleSearch, Keys.SHIFT)
+                .build();
+        searchYohji.perform();
+
+        String expectedTitle = "yohji - Google Search";
+        String pageTitle = driver.getTitle();
+        Assert.assertEquals(expectedTitle, pageTitle);
+        Thread.sleep(2000);
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0, 600)");
+        Thread.sleep(2000);
+
+        WebElement theShop = driver.findElement(By.xpath("//h3[contains(text(), 'THE SHOP YOHJI')]"));
+        Action enterShop = builder
+                .moveToElement(theShop)
+                .click()
+                .build();
+        enterShop.perform();
+
+        expectedTitle = "THE SHOP YOHJI YAMAMOTO";
+        pageTitle = driver.getTitle();
+        Assert.assertEquals(expectedTitle, pageTitle);
+        Thread.sleep(3000);
     }
 
     @Test
